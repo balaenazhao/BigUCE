907fbd298eaa9f03aae5fed053a0996c60615b80
Min Zhao <42870954+balaenazhao@users.noreply.github.com>
2024-04-09 11:54:59 -0400

    Update Figure S8 - Fully Stratified Subsets.R

--- Visualization/supplementary/Figure S8 - Fully Stratified Subsets.R
+++ Visualization/supplementary/Figure S8 - Fully Stratified Subsets.R
@@ -1,53 +1,32 @@
-#===============================================================================
-# Libraries
-#===============================================================================
 library(ComplexHeatmap)
 library(magrittr)
 library(dplyr)
 library(readr)
 
-#===============================================================================
-# Data
-#===============================================================================
-
-#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-# Occurrence
-#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+####### read in data file that contains 2670 taxa and the subsets they belong #######
+####### present: 1; absent: NA #######
 mat <- read_csv("./subsets.csv")
-
-mat %>% head()
-
 gr <- colnames(mat)[2:ncol(mat)]
 tx <- mat$taxon
-
 mt <- mat[,2:ncol(mat)] %>% 
   t() %>%
   as.matrix()
-
 colnames(mt) <- 1:ncol(mt)
 rownames(mt) <- gr
+mt[is.na(mt)] <- 0 # replace NAs with O
 
-mt[1:5,1:5]
-
-mt[is.na(mt)] <- 0
-
-#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-# Sample group information
-#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+####### read in subset taxonomic group information #######
 group_info <- readxl::read_xlsx("./group_info.xlsx")
-
 order_names <- str_replace(tx,"_.*","")
 
-#===============================================================================
-# Heatmap
-#===============================================================================
+####### create presence/absence matrix to be drawn as a heatmap #######
 ha <- data.frame("order" = order_names) %>%
   dplyr::left_join(group_info) %>%
   dplyr::mutate(group = fct_relevel(group, unique(group_info$group)))
 
 rownames(ha) <- 1:ncol(mt)
 
-# Color Palette
+####### apply selected color palette #######
 col_pal <- c(
   "OUTGROUP" = "black",
   "Palaeognathae" = "#875692",
@@ -71,7 +50,7 @@ col_pal <- c(
   `PASSERIFORMES-oscine` = "#52c9daff"
 )
 
-# Top color bar
+####### Top color bar #######
 top_ha <- HeatmapAnnotation(
   df = ha %>% dplyr::select(group),
   col = list(group = col_pal),
@@ -82,7 +61,7 @@ top_ha <- HeatmapAnnotation(
   )
 )
 
-# Draw heatmap
+####### Draw heatmap #######
 pdf("./subset_occurrence.pdf", width = 12, height = 8)
 ht <- Heatmap(matrix = mt,
         col = c("white","#3483b1ff"),
