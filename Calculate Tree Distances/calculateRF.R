b3669d4bc972fb339588b1f6e0f5ae5f6db1afd8
Min Zhao <42870954+balaenazhao@users.noreply.github.com>
2025-01-09 15:45:22 -0500

    Update calculateRF.R

--- Calculate Tree Distances/calculateRF.R
+++ Calculate Tree Distances/calculateRF.R
@@ -4,14 +4,13 @@ library(phytools)
 library(TreeDist)
 
 #### after running ete3 ###
-data<- read.csv("RF.csv",header=TRUE)
+data<- read.csv("RF_improved_methods_V2.csv",header=TRUE)
 matrix<- xtabs(nRF ~ source + reference, data=data)
 
 dist<- as.dist(matrix)
 
-write.nexus.dist(dist, file = "Robinson_Foulds_distances.nex", append = FALSE, upper = FALSE,
+write.nexus.dist(dist, file = "RF_matrix_improved_methods_V2.nex", append = FALSE, upper = FALSE,
                  diag = TRUE, digits = getOption("digits"), taxa = TRUE)
 
 #### estimate NJ tree using PAUP* ###
-
-
+#### paup NJ_distance.nex
